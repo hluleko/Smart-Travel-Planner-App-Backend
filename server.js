8221affea815ac35ddad9fdcf81727e1fb45b7c3
hluleko <130248982+hluleko@users.noreply.github.com>
2025-04-15 11:36:11 +0200

    done

--- server.js
+++ server.js
@@ -301,7 +301,18 @@ app.post("/review", authenticateToken, async (req, res) => {
     res.status(500).json({ error: "Failed to submit review." });
   }
 });
+function authenticateToken(req, res, next) {
+  const token = req.headers.authorization;
+  if (!token) return res.status(401).json({ error: "Unauthorized." });
 
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(403).json({ error: "Invalid token." });
+  }
+}
 
 // Start Server
 const PORT = process.env.PORT || 5000;
